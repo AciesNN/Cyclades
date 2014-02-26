33767021f9bb0e1040bcf8dcd839efe59440357d
Acies <acies4spam@mail.ru>
2014-02-26 11:18:48 +0400

    отладочные кнопки текущего игрока

--- Assets/Scripts/UI/GameScene/Admin/LoadData.cs
+++ Assets/Scripts/UI/GameScene/Admin/LoadData.cs
@@ -5,9 +5,38 @@ using Shmipl.Unity;
 
 public class LoadData : MonoBehaviour {
 	public UIInput fileName;
-		
+
+	void UpdateView() {
+		GetComponent<UICollectionController>().UpdateView();
+	}
+
 	void DataLoad() {
 		data.LoadData((fileName.value == "" ? fileName.defaultText : fileName.value));
-		GetComponent<UICollectionController>().UpdateView();
+		UpdateView();
+	}
+
+	void SetCurPlayer(long p) {
+		data.cur_player = p; 
+		UpdateView();
+	}
+
+	public void SetCurPlayer1() {
+		SetCurPlayer(1);
+	}
+
+	public void SetCurPlayer2() {
+		SetCurPlayer(2);
+	}
+
+	public void SetCurPlayer3() {
+		SetCurPlayer(3);
+	}
+
+	public void SetCurPlayer4() {
+		SetCurPlayer(4);
+	}
+
+	public void SetCurPlayer5() {
+		SetCurPlayer(5);
 	}
 }
