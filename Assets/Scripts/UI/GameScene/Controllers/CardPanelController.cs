4d40916521bf52177e6eff600f202a6c6801b9a0
Acies <acies4spam@mail.ru>
2014-03-03 15:41:49 +0400

    зачатки панели карт

---
+++ Assets/Scripts/UI/GameScene/Controllers/CardPanelController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Cyclades.Game;
+using Shmipl.Unity;
+
+namespace Shmipl.GameScene
+{
+	public class CardPanelController : UIController {
+
+		public UIButton[] cards;
+
+		private void OpenCardEnable_UpdateView () {
+
+		}
+
+		public override void UpdateView () {
+			OpenCardEnable_UpdateView();
+		}
+		
+		public void BuyCard(long card) {
+			Hashtable msg = Client.BuyCard(card);
+			Debug.Log("msg: " + Shmipl.Base.json.dumps(msg));
+		}
+
+		/*сигналы*/
+		void BuyCard0() {
+			BuyCard(0);
+		}
+
+		void BuyCard1() {
+			BuyCard(1);
+		}
+
+		void BuyCard2() {
+			BuyCard(2);
+		}
+	}
+}
