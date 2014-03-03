08f56b1e15c1b3a2d69c3011c562d17608f7c726
Acies <acies4spam@mail.ru>
2014-03-03 12:01:37 +0400

    начаты панели богов: софии и зевса

---
+++ Assets/Scripts/UI/GameScene/Controllers/GodSophia.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Cyclades.Game;
+using Shmipl.Unity;
+
+namespace Shmipl.GameScene
+{
+	public class GodSophia : UIController {
+
+		public override void UpdateView () {
+
+		}
+
+		public void BuyPhilosopher() {
+			Hashtable msg = Client.BuyPhilosopher();
+			Debug.Log("msg: " + Shmipl.Base.json.dumps(msg));
+		}
+	}
+}
---
+++ Assets/Scripts/UI/GameScene/Controllers/GodZeus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Cyclades.Game;
+using Shmipl.Unity;
+
+namespace Shmipl.GameScene
+{
+	public class GodZeus : UIController {
+		
+		public override void UpdateView () {
+			
+		}
+		
+		public void BuyPriest() {
+			Hashtable msg = Client.BuyPriest();
+			Debug.Log("msg: " + Shmipl.Base.json.dumps(msg));
+		}
+	}
+}
