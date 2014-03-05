9151afa74812b425778de8ac7bded2e537332c59
Владислав Прыгин <prygin.vlad@yandex.ru>
2014-03-06 00:12:40 +0400

    Столько трудов вроде бы увенчались успехом. Сцена Testing-это сцена с демонстранией генерации карты и её дрэг н дропом.

---
+++ Assets/Scripts/test/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+	public UISprite[] mapTile;
+	public string[] tileID;
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i<mapTile.Length; i++) {
+			mapTile[i].spriteName=tileID[i];
+
+
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
