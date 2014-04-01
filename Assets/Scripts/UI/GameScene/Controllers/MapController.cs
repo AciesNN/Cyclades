350c48930edd383b54fc371a4e7ce12e02b0e514
Acies <acies4spam@mail.ru>
2014-04-01 13:51:40 +0400

    MapController клики на карте - пока с глюком в математике

---
+++ Assets/Scripts/UI/GameScene/Controllers/MapController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+	GridController grid;
+
+	// Use this for initialization
+	void Start () {
+		grid = GameObject.Find("grid").GetComponent<GridController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick() {
+		Debug.Log("OnClick");
+		Vector3 pos;
+		if (grid.MousePointToColliderHitPosition(out pos)) {				
+			Vector2 cell = grid.WorldPositionToCell(pos);
+			Debug.Log(cell);
+		}
+	}
+}
