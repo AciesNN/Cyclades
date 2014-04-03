5dd82696efe08e059e1de61b9d2c29e5fa968826
Acies <acies4spam@mail.ru>
2014-04-03 16:38:33 +0400

    мини

--- Assets/Scripts/ShmiplUnity/TerrainHeightsLoader.cs
+++ Assets/Scripts/ShmiplUnity/TerrainHeightsLoader.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace Shmipl.Unity
 {
 	public class TerrainHeightsLoader {
-		static readonly float heightCoef = 0.01f;
+		static readonly float maxHeight = 0.01f; //это волшебная высота (todo - выяснить, от чего зависит данный, коэф-нт!)
 
 		/*загружает карту высот из градиента серого картинки
 		 * внимание, надо сделать текстуру читаемой (для чего поставить тип текстуры advanced)
@@ -12,14 +12,14 @@ namespace Shmipl.Unity
 		public static void LoadHeighMapFromTexture(Texture2D texture, Terrain terrain) {
 			float[,] heights = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution);
 			
-			//хитрость с координатами сделана потому, что матрица 
+			//хитрость с координатами сделана потому, что матрица террейна считается снизу справа, а точки картинки - сверху слева
 			for (int y = terrain.terrainData.heightmapResolution-1; y >= 0; --y)
 			{
 				for (int x = 0; x < terrain.terrainData.heightmapResolution; ++x)
 				{
 					if (y < texture.height && x < texture.width) {
 						float height = 1.0f - texture.GetPixel(x, terrain.terrainData.heightmapResolution - y - 1).grayscale;
-						heights[x, y] = heightCoef * height; //это волшебная высота, на нее надо домножать цвет картинки (выяснить, от чего зависит данный, коэф-нт! )
+						heights[x, y] = maxHeight * height;
 					} else {
 						heights[x, y] = 0.0f;
 					}
