71ce82e98f63f455bf2f3fb76ea681197f004311
Acies <acies4spam@mail.ru>
2014-04-11 15:23:42 +0400

    -

---
+++ Assets/Scripts/UI/GameScene/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Shmipl;
+
+public class GameController  {
+
+	Shmipl.Base.FSM fsm;
+
+	public GameController() {
+		fsm = new Shmipl.Base.FSM();
+
+		//fsm.AddStringToTable(
+	}
+
+}
