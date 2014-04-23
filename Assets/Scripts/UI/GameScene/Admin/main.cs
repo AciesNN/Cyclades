5c903331b164237a527f521fdd7a9267111212d9
Acies <acies4spam@mail.ru>
2014-04-23 16:05:32 +0400

    -

--- Assets/Scripts/UI/GameScene/Admin/main.cs
+++ Assets/Scripts/UI/GameScene/Admin/main.cs
@@ -28,6 +28,8 @@ namespace Shmipl.GameScene
 					//TODO!!! тут вот вообще нехорошо! убрать! пользуемся вредной функцией
 					return Cyclades.Program.srv.GetContext("Game");
 				}
+				if (Cyclades.Program.clnts[(int)Cyclades.Game.Client.Messanges.cur_player].GetContext("Game") == null)
+					return Cyclades.Program.srv.GetContext("Game"); //TODO - явная недоработка синхронизации!
 				return Cyclades.Program.clnts[(int)Cyclades.Game.Client.Messanges.cur_player].GetContext("Game");
 			}
 		}
