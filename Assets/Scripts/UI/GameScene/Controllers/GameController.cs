002bff9f7f21aeb0de2a19c693e266f34144972c
Acies <acies4spam@mail.ru>
2014-05-08 09:42:15 +0400

    постройка метрополии за счет зданий

--- Assets/Scripts/UI/GameScene/Controllers/GameController.cs
+++ Assets/Scripts/UI/GameScene/Controllers/GameController.cs
@@ -64,11 +64,59 @@ namespace Shmipl.GameScene
 		}
 
 		void OnMapClick_PlaceMetroBuilding(Coords coords) {
-			List<object> slots = new List<object>(); //TODO пока никак не определяются сносимые здания
+			List<object> slots = GetListOfSlotsOfPlaceMetroBuilding(Library.Map_GetIslandByPoint(main.instance.context, coords.x, coords.y), Cyclades.Game.Client.Messanges.cur_player); //TODO пока как-либо определяются сносимые здания
 			main.instance.SendSrv( Cyclades.Game.Client.Messanges.PlaceMetro4Buildings(Library.Map_GetIslandByPoint(main.instance.context, coords.x, coords.y), slots) );
 			
 			main.instance.game.gameMode = GameMode.simple;
 			Shmipl.Base.Messenger<Coords>.RemoveListener("Shmipl.Map.Click", OnMapClick_PlaceMetroBuilding);
+		}
+
+
+
+
+
+		/*TODO временная ф-ция дает список зданий, которые можно снести под метрополию*/
+		List<object> GetListOfSlotsOfPlaceMetroBuilding(long firstIsland, long owner) {
+			List<object> res = new List<object>();
+			Dictionary<string, bool> _get_builds = new Dictionary<string, bool>() { {Cyclades.Game.Constants.buildMarina, false}, {Cyclades.Game.Constants.buildUniver, false}, {Cyclades.Game.Constants.buildFortres, false}, {Cyclades.Game.Constants.buildTemple, false}}; //временная, для отметки уже полученных зданий
+
+			List<object> buildings = main.instance.context.GetList("/map/islands/buildings");
+			List<long> owners = main.instance.context.GetList<long>("/map/islands/owners");
+
+			List<object> builds_on_island;
+			long slot;
+
+			//1. подчистим все с самого острова
+			builds_on_island = buildings[(int)firstIsland] as List<object>;
+
+			slot = 0;
+			foreach(string b in builds_on_island) {
+				if (b != Cyclades.Game.Constants.buildNone && _get_builds[b] != true) {
+					res.Add ( new List<object>() {firstIsland, slot} );
+					_get_builds[b] = true;
+				}
+				++slot;
+			}
+
+			//2. тоже самое для всех остальных
+			for(long island = 0; island < owners.Count; ++island) {
+				if (island == firstIsland)
+					continue;
+				if (owners[(int)island] != owner)
+					continue;
+
+				builds_on_island = buildings[(int)island] as List<object>;
+				slot = 0;
+				foreach(string b in builds_on_island) {
+					if (b != Cyclades.Game.Constants.buildNone && _get_builds[b] != true) {
+						res.Add ( new List<object>() {island, slot} );
+						_get_builds[b] = true;
+					}
+					++slot;
+				}			
+			}
+
+			return res;
 		}
 	}
 }
